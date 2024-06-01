f61258e0104b307ac4a9b9e98c77dbedbe8af7a8
Baher Talal <baher692@gmail.com>
2024-06-01 12:00:43 +0300

    add bootstrap to response.php

--- response.php
+++ response.php
@@ -4,26 +4,30 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Meeting Requests</title>
+<!-- Link Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
 
 <h2>Meeting Requests</h2>
 
-<table id="meetingTable" border="1">
-  <thead>
-    <tr>
-      <!-- <th>Meeting ID</th> -->
-      <th>Title</th>
-      <th>Date</th>
-      <th>Time</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <!-- Meeting requests will be inserted here -->
-  </tbody>
-</table>
+<div class="container">
+  <table id="meetingTable" class="table table-bordered">
+    <thead>
+      <tr>
+        <!-- <th>Meeting ID</th> -->
+        <th>Title</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- Meeting requests will be inserted here -->
+    </tbody>
+  </table>
+</div>
 
 <script>
 $(document).ready(function(){
@@ -38,10 +42,10 @@ $(document).ready(function(){
       tr.append('<td>' + meeting.date + '</td>');
       tr.append('<td>' + meeting.time + '</td>');
       var td = $('<td>');
-      var acceptBtn = $('<button>').text('Accept').click(function(){
+      var acceptBtn = $('<button>').addClass('btn btn-success').text('Accept').click(function(){
         updateResponse(meeting.id, 'accepted');
       });
-      var rejectBtn = $('<button>').text('Reject').click(function(){
+      var rejectBtn = $('<button>').addClass('btn btn-danger').text('Reject').click(function(){
         updateResponse(meeting.id, 'rejected');
       });
       td.append(acceptBtn).append(rejectBtn);
@@ -58,7 +62,7 @@ $(document).ready(function(){
       data: JSON.stringify({ id: id, response: response }),
       contentType: 'application/json',
       success: function(data){
-        alert("your response is saved");
+        alert("Your response is saved");
         location.reload();
         // Optionally, you can remove the row from the table upon successful response
         // $('#meetingTable tbody tr').remove(':contains("' + id + '")');
